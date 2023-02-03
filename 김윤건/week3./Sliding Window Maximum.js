d3ca8a93ca77cc6cabef57017b82e52ba72c7fd3
younchong <ungun96@naver.com>
2023-02-03 12:27:00 +0900

    week2 monotonic deque

--- 김윤건/week3./Sliding Window Maximum.js
+++ 김윤건/week3./Sliding Window Maximum.js
@@ -14,3 +14,29 @@ var maxSlidingWindow = function(nums, k) {
 
   return answer;
 };
+// time limit
+
+var maxSlidingWindow = function(nums, k) {
+  const result = [];
+  const q = [];
+
+  for (let i = 0; i < nums.length; i++) {
+      while (q && nums[q[q.length - 1]] <= nums[i]) {
+          q.pop();
+      }
+
+      q.push(i);
+
+      if (q[0] === i - k) {
+          q.shift();
+      }
+
+      if (i >= k - 1) {
+          result.push(nums[q[0]]);
+      }
+  }
+
+  return result;
+};
+
+// monotonic deque

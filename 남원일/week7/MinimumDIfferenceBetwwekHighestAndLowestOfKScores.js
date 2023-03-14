bcee4a3c6fbd40bb98265ca1d11f3bcf2c8b5970
Wonil Nam <wonil.nam@voyagerx.com>
2023-03-14 09:54:11 +0900

    [남원일][7주차]Minimum Difference Between Highest and Lowest of K Scores 풀이 refactoring 2nd

--- 남원일/week7/MinimumDIfferenceBetwwekHighestAndLowestOfKScores.js
+++ 남원일/week7/MinimumDIfferenceBetwwekHighestAndLowestOfKScores.js
@@ -8,9 +8,6 @@ var minimumDifference = function (nums, k) {
   let result;
   let currentIndex = 0;
 
-  if (nums.length === k)
-    return sortedNums[sortedNums.length - 1] - sortedNums[0];
-
   while (currentIndex + k - 1 < nums.length) {
     const differenceBetweenMaxAndMin =
       sortedNums[currentIndex + k - 1] - sortedNums[currentIndex];

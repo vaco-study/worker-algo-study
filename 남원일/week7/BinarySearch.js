cbd6958a294149da1fc25929d367f95f72656266
Wonil Nam <wonil.nam@voyagerx.com>
2023-03-16 10:00:14 +0900

    [남원일][7주차]Binary Search 모범답안 추가

--- 남원일/week7/BinarySearch.js
+++ 남원일/week7/BinarySearch.js
@@ -27,3 +27,26 @@ var search = function (nums, target) {
 
   return -1;
 };
+
+// 좋은 풀이 , 모범 답안
+
+const search = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+};

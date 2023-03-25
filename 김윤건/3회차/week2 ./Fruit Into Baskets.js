278ea35e12ca2880c24aa1572e7677073a98f19c
younchong <ungun96@naver.com>
2023-03-25 22:22:49 +0900

    week2.

--- 김윤건/3회차/week2 ./Fruit Into Baskets.js
+++ 김윤건/3회차/week2 ./Fruit Into Baskets.js
@@ -2,31 +2,31 @@
  * @param {number[]} fruits
  * @return {number}
  */
- var totalFruit = function(fruits) {
-  if (new Set(fruits).size <= 2) return fruits.length;
+var totalFruit = function(fruits) {
+    if (new Set(fruits).size <= 2) return fruits.length;
 
-  const baskets = new Map();
-  let left = 0;
-  let max = 0;
-  
-  for (let right = 0; right < fruits.length; right++) {
-      const fruit = fruits[right];
-      baskets.set(fruit, baskets.get(fruit) + 1 || 0);
+    const baskets = new Map();
+    let left = 0;
+    let max = 0;
 
-      while (baskets.size > 2) {
-          const prevFruit = fruits[left];
+    for (let right = 0; right < fruits.length; right++) {
+        const fruit = fruits[right];
+        baskets.set(fruit, baskets.get(fruit) + 1 || 0);
 
-          if (baskets.get(prevFruit) === 0) {
-              baskets.delete(prevFruit);
-              console.log(baskets)
-          } else {
-              baskets.set(prevFruit, baskets.get(prevFruit) - 1);
-          }
-          left++;
-      }
+        while (baskets.size > 2) {
+            const prevFruit = fruits[left];
 
-      max = Math.max(max, right - left + 1);
-  }
+            if (baskets.get(prevFruit) === 0) {
+                baskets.delete(prevFruit);
+                console.log(baskets)
+            } else {
+                baskets.set(prevFruit, baskets.get(prevFruit) - 1);
+            }
+            left++;
+        }
 
-  return max;
+        max = Math.max(max, right - left + 1);
+    }
+
+    return max;
 };
